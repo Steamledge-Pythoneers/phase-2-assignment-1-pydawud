340a1da2f0f72c032519efa5b1519ceeceeb6d20
Dauda Sani Abdullahi <daudasaniaa008@gmail.com>
2020-09-28 12:10:09 +0100

    compute lowest term of non zero numbers

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,4 +1,30 @@
 ## TODO: complete the function "lowest_terms" below
 
 def lowest_terms(x):
-	return ""
+	'''
+	compute the lowest term of a fraction
+	:param x: string containing a fraction
+	:return: reduce form of x
+	'''
+	numerator, denominator = map(int, x.split('/'))
+	def greatest_common_divisor(n, d):
+		while d !=0:
+			temp = d
+			d = n%d
+			n = temp
+
+		return n
+	greatest = greatest_common_divisor(numerator, denominator)
+	numerator /= greatest
+	denominator /= greatest
+
+	#print(numerator,denominator)
+	return "{}/{}".format(int(numerator),int(denominator))
+
+#lowest_terms('20/10')
+print(lowest_terms('20/10'))
+print(lowest_terms('16/28'))
+print(lowest_terms('-50/25'))
+print(lowest_terms('20/-60'))
+print(lowest_terms('-300/165'))
+print(lowest_terms('-12/-26'))
