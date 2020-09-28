a33f1a43bf5d5d4e02bd90b198bcfc43a1343cd5
Dauda Sani Abdullahi <daudasaniaa008@gmail.com>
2020-09-28 12:30:11 +0100

    Handle division by zero.
    All codes used in this assignment is composed through research from multiple sources and their is no plagiarised code.

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -6,25 +6,43 @@ def lowest_terms(x):
 	:param x: string containing a fraction
 	:return: reduce form of x
 	'''
-	numerator, denominator = map(int, x.split('/'))
+
 	def greatest_common_divisor(n, d):
+		'''
+		find the largest factor divisible by n and d
+		:param n: integer
+		:param d: integer
+		:return: return the greatest common factor of n and d
+		'''
+
 		while d !=0:
 			temp = d
 			d = n%d
 			n = temp
 
 		return n
-	greatest = greatest_common_divisor(numerator, denominator)
-	numerator /= greatest
-	denominator /= greatest
 
-	#print(numerator,denominator)
-	return "{}/{}".format(int(numerator),int(denominator))
+	numerator, denominator = map(int, x.split('/'))
 
-#lowest_terms('20/10')
+	#checking for 'undefined' and numerator of zero
+	if numerator == 0:
+		result = '0'
+	elif denominator == 0:
+		result = 'Undefined'
+	else:
+		greatest = greatest_common_divisor(numerator, denominator)
+		numerator /= greatest
+		denominator /= greatest
+
+		result = "{}/{}".format(int(numerator),int(denominator))
+
+	return result
+
 print(lowest_terms('20/10'))
 print(lowest_terms('16/28'))
 print(lowest_terms('-50/25'))
 print(lowest_terms('20/-60'))
 print(lowest_terms('-300/165'))
 print(lowest_terms('-12/-26'))
+print(lowest_terms('0/26'))
+print(lowest_terms('12/0'))
